ada778b7794bd1ea5deb3c91d24366a19f666a33
glavadoj01 <gonzalolj@hotmail.es>
2025-06-12 13:42:59 +0200

    Clase 180 => Páginas básicas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes')
+  },
+  {
+    path: 'reactive',
+    loadChildren: () => import('./reactive/reactive.routes').then(m => m.reactiveRoutes)
+  },
+  {
+    path: 'country',
+    loadChildren: () => import('./country/country.routes').then(m => m.cuntryRoutes)
+  },
+  {
+    path: '**',
+    redirectTo: 'reactive',
+  }
+];
---
+++ src/app/reactive/reactive.routes.ts
@@ -0,0 +1,30 @@
+import { Routes } from "@angular/router";
+import { BasicPageComponent } from "./pages/basic-page/basic-page.component";
+import { DynamicPageComponent } from "./pages/dynamic-page/dynamic-page.component";
+
+export const reactiveRoutes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'basic',
+        title: 'Básicos',
+        component: BasicPageComponent
+      },
+      {
+        path: 'dynamic',
+        title: 'Dinámicos',
+        component: DynamicPageComponent
+      },
+      {
+        path: 'switches',
+        title: 'Switches',
+        component: DynamicPageComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'basic'
+      }
+    ]
+  }
+]
